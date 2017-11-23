c9b46d0228ac33b405b79fb4ec9c8814245c84ad
Kirill Avramenko <mirecsy@gmail.com>
2017-11-23 13:35:53 -0800

    Add project

---
+++ DebugLogHelper/Assets/Shorka/DebugHelper/DebugHelper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shorka
+{
+
+    public enum DebugColors
+    {
+        aqua,
+        black,
+        blue,
+        brown,
+        cyan,
+        darkblue,
+        fuchsia,
+        green,
+        grey,
+        lightblue,
+        lime,
+        magenta,
+        maroon,
+        navy,
+        olive,
+        purple,
+        red,
+        silver,
+        teal,
+        white,
+        yellow
+    }
+
+    public static class DebugHelper 
+    {
+
+        /// <summary>
+        /// Sets the color of the text according to the parameter value.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="color">Color.</param>
+        public static string StrColored(this string message, DebugColors color)
+        {
+            return string.Format("<color={0}>{1}</color>", color.ToString(), message);
+        }
+
+        /// <summary>
+        /// Sets the size of the text according to the parameter value, given in pixels.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="size">Size.</param>
+        public static string StrSized(this string message, int size)
+        {
+            return string.Format("<size={0}>{1}</size>", size, message);
+        }
+
+        /// <summary>
+        /// Renders the text in boldface.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static string StrBold(this string message)
+        {
+            return string.Format("<b>{0}</b>", message);
+        }
+
+        /// <summary>
+        /// Renders the text in italics.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static string StrItalics(this string message)
+        {
+            return string.Format("<i>{0}</i>", message);
+        }
+    }
+
+}
+
---
+++ DebugLogHelper/Assets/Shorka/DebugHelper/Example/LogShower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shorka
+{
+    public class LogShower : MonoBehaviour
+    {
+        
+        void Start()
+        {
+            Debug.Log("This is " + "colored log".StrColored(DebugColors.blue));
+            Debug.Log("This is " + "italics log".StrItalics());
+            Debug.Log("This is " + "bold log".StrBold());
+            Debug.Log("This is " + "bold & colored log".StrBold().StrColored(DebugColors.purple));
+            Debug.Log("This is " + "sized log".StrSized(17));
+        }        
+    }
+}
